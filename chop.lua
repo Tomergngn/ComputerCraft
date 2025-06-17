e1a9c8af3db0b7036fd014c4e77994c026a9c02f
Tomergngn <tomer.gal.netser@gmail.com>
2025-06-17 18:43:55 +0300

    Compi :)

---
+++ chop.lua
@@ -0,0 +1,17 @@
+-- Checking if the tree grew:
+turtle.select(16)
+if turtle.compare() then
+    turtle.dig()
+    turtle.forward()
+    turtle.select(15)
+    local height = 0
+    while !turtle.compare() do
+        turtle.digUp()
+        turtle.up()
+        height = height + 1
+    end
+    for i = 1, height do
+        turtle.down()
+    end
+    turtle.back()
+end
