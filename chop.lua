c910f6cace05fe72c466f62b17161d9f4c32feba
Tomergngn <tomer.gal.netser@gmail.com>
2025-06-17 18:56:13 +0300

    C

--- chop.lua
+++ chop.lua
@@ -1,7 +1,23 @@
+function FuelUp()
+    -- Check if the turtle has enough fuel
+    if turtle.getFuelLevel() < 10 then
+        local slct = turtle.getSelectedSlot()
+        turtle.select(14)
+        if not turtle.refuel() then
+            debug_print("Not enough fuel! Please refuel the turtle.")
+            turtle.select(slct)
+            return false
+        end
+        turtle.select(slct)
+        return true
+    end
+end
+
 -- Checking if the tree grew:
 turtle.select(16)
 if turtle.compare() then
     turtle.dig()
+    FuelUp()
     turtle.forward()
     turtle.select(15)
     local height = 0
