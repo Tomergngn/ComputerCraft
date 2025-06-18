0e6ee555fb0a0d1e1d37f0be016473422ef0572e
Tomergngn <tomer.gal.netser@gmail.com>
2025-06-18 03:13:19 +0300

    Update harvestleaves.lua

--- harvestleaves.lua
+++ harvestleaves.lua
@@ -47,6 +47,14 @@ function BreakLeaves()
     turtle.forward()
     turtle.forward()
     turtle.turnRight()
+
+    for i = 1, 16 do
+        if turtle.getItemDetail(i) then
+            turtle.select(i)
+            turtle.dropDown()
+        end
+    end
+
     for _=1, (3+shortTree) do turtle.up() end
     turtle.back()
     turtle.back()
@@ -60,14 +68,8 @@ while true do
     repeat
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == 1
+
     Modem.transmit(replyChannel, 1, true)
     BreakLeaves()
-
-    for i = 1, 16 do
-        if turtle.getItemDetail(i) then
-            turtle.select(i)
-            turtle.dropDown()
-        end
-    end
     Modem.transmit(replyChannel, 1, false)
 end
