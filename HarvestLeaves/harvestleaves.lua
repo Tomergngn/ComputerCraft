775d1524fc1f910cfb0bf90cd7d9eb7f1e4b2df9
Tomergngn <tomer.gal.netser@gmail.com>
2025-06-18 18:48:04 +0300

    Update harvestleaves.lua

--- HarvestLeaves/harvestleaves.lua
+++ HarvestLeaves/harvestleaves.lua
@@ -1,85 +1,78 @@
 local Locator = {}
-Locator.new = function()
-    local self = {}
 
-    self.upMov = 0
-    self.rightRot = 0
-    self.backMov = 0
-    self.leftMov = 0
-
-    self.update = function() SaveState:write(self.rightRot .. "\n" .. self.backMov .. "\n" .. self.leftMov .. "\n" .. self.upMov .. "\n") SaveState:flush() end
-
-    self.forward = function()
-        if turtle.forward() then
-            if self.rightRot == 0 then
-                self.backMov = self.backMov - 1
-            elseif self.rightRot == 1 then
-                self.leftMov = self.leftMov - 1
-            elseif self.rightRot == 2 then
-                self.backMov = self.backMov + 1
-            elseif self.rightRot == 3 then
-                self.leftMov = self.leftMov + 1
-            end
-            self.update()
-            return true
-        end
-        return false
-    end
-    self.back = function()
-        if turtle.back() then
-            if self.rightRot == 0 then
-                self.backMov = self.backMov + 1
-            elseif self.rightRot == 1 then
-                self.leftMov = self.leftMov + 1
-            elseif self.rightRot == 2 then
-                self.backMov = self.backMov - 1
-            elseif self.rightRot == 3 then
-                self.leftMov = self.leftMov - 1
-            end
-            self.update()
-            return true
-        end
-        return false
-    end
-    self.up = function()
-        if turtle.up() then
-            self.upMov = self.upMov + 1
-            self.update()
-            return true
-        end
-        return false
-    end
-    self.down = function()
-        if turtle.down() then
-            self.upMov = self.upMov - 1
-            self.update()
-            return true
-        end
-        return false
-    end
-    self.turnLeft = function()
-        Locator.turnLeft()
-        self.rightRot = (self.rightRot - 1) % 4
-        self.update()
-    end
-    self.turnRight = function()
-        turtle.turnRight()
-        self.rightRot = (self.rightRot + 1) % 4
-        self.update()
-    end
-    return self
+function Locator:update()
+    SaveState:write(self.rightRot .. "\n" .. self.backMov .. "\n" .. self.upMov .. "\n")
+    SaveState:flush()
 end
 
+function Locator:forward()
+    if turtle.forward() then
+        if self.rightRot == 0 then
+            self.backMov = self.backMov - 1
+        elseif self.rightRot == 1 then
+            self.leftMov = self.leftMov - 1
+        elseif self.rightRot == 2 then
+            self.backMov = self.backMov + 1
+        elseif self.rightRot == 3 then
+            self.leftMov = self.leftMov + 1
+        end
+        Locator:update()
+        return true
+    end
+    return false
+end
+function Locator:back()
+    if turtle.back() then
+        if self.rightRot == 0 then
+            self.backMov = self.backMov + 1
+        elseif self.rightRot == 1 then
+            self.leftMov = self.leftMov + 1
+        elseif self.rightRot == 2 then
+            self.backMov = self.backMov - 1
+        elseif self.rightRot == 3 then
+            self.leftMov = self.leftMov - 1
+        end
+        Locator:update()
+        return true
+    end
+    return false
+end
+function Locator:up()
+    if turtle.up() then
+        Locator.upMov = Locator.upMov + 1
+        Locator:update()
+        return true
+    end
+    return false
+end
+function Locator:down()
+    if turtle.down() then
+        Locator.upMov = Locator.upMov - 1
+        Locator:update()
+        return true
+    end
+    return false
+end
+function Locator:turnLeft()
+    turtle.turnLeft()
+    Locator.rightRot = (Locator.rightRot - 1) % 4
+    Locator:update()
+end
+function Locator:turnRight()
+    turtle.turnRight()
+    Locator.rightRot = (Locator.rightRot + 1) % 4
+    Locator:update()
+end
 function BreakLeaves()
     function BreakTriplet()
         turtle.dig()
-        turtle.forward()
+        Locator:forward()
         turtle.digUp()
         turtle.digDown()
     end
     function BreakDouble()
         turtle.dig()
-        turtle.forward()
+        Locator:forward()
         turtle.digDown()
     end
     local shortTree = 0
@@ -91,34 +84,34 @@ function BreakLeaves()
     for _=1, 3 do turtle.down() end
     
     BreakTriplet()
-    turtle.turnLeft()
+    Locator:turnLeft()
     BreakTriplet()
 
     for _=1, 3 do
-        turtle.turnLeft()
+        Locator:turnLeft()
         BreakTriplet()
         BreakTriplet()
     end
 
     BreakDouble()
 
-    turtle.turnLeft()
+    Locator:turnLeft()
     BreakDouble()
     BreakDouble()
     BreakDouble()
 
     for _=1, 3 do
-        turtle.turnLeft()
+        Locator:turnLeft()
         BreakDouble()
         BreakDouble()
         BreakDouble()
         BreakDouble()
     end
 
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.forward()
-    turtle.turnRight()
+    Locator:turnLeft()
+    Locator:forward()
+    Locator:forward()
+    Locator:turnRight()
 
     for i = 1, 16 do
         if turtle.getItemDetail(i) then
@@ -128,8 +121,8 @@ function BreakLeaves()
     end
 
     for _=1, (3+shortTree) do turtle.up() end
-    turtle.back()
-    turtle.back()
+    Locator:back()
+    Locator:back()
 end
 
 SaveState = io.open("savestate.txt", "w")
